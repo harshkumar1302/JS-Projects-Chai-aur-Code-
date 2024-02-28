0439f03c383bc2b603a8c5f080ec661e3269b8f0
harshkumar1302 <bs824536@gmail.com>
2024-02-28 17:01:02 +0530

    P2

--- 02_BMICalculator/script.js
+++ 02_BMICalculator/script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 // console.log(form);
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
     const height = parseInt(document.querySelector("#height").value);
     // console.log(height);
@@ -14,14 +14,14 @@ form.addEventListener('submit', (event) => {
         results.innerHTML = `Please give a valid weight in number = ${weight}`;
     } else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-        results.innerHTML = `<span>${bmi}</span>`
+        // results.innerHTML = `<span>${bmi}</span>`;
 
         if (bmi < 18.6) {
-            results.innerHTML = `<span>${bmi}</span> <p>You are under weight.</p>`
+            results.innerHTML = `<span>${bmi}</span> = You are under weight.`
         } else if (bmi >= 18.6 && bmi <= 24.9) {
-            results.innerHTML = `<span>${bmi}</span> <p>you fall in Normal Range.</p>`
+            results.innerHTML = `<span>${bmi}</span> = you fall in Normal Range.`
         } else if (bmi > 24.9) {
-            results.innerHTML = `<span>${bmi}</span> <p>you are OverWeight.</p>`
+            results.innerHTML = `<span>${bmi}</span> = you are OverWeight.`
         }
     }
 });
