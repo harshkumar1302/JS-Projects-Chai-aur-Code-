503d346dfd8424184e82aca3fbec449bc035ab0e
harshkumar1302 <bs824536@gmail.com>
2024-02-28 16:42:40 +0530

    P1

--- 01_Color-Changer/script.js
+++ 01_Color-Changer/script.js
@@ -1,40 +1,42 @@
 const buttons = document.querySelectorAll(".button");
 // console.log(buttons);
+
 const body = document.querySelector("body");
-// console.log(body)
+// console.log(body);
 
 
 buttons.forEach((button) => {
-    console.log(button);
-    button.addEventListener("click", (event) => {
-        // console.log(event);
-        // console.log(event.target);
+    // console.log(button);
+    button.addEventListener("click", function (e) {
+        // console.log(e);
+        // console.log(e.target);
 
-        if (event.target.id === "grey") {
-            body.style.backgroundColor = event.target.id
-        } else if (event.target.id === "white") {
-            body.style.backgroundColor = event.target.id
-        } else if (event.target.id === "blue") {
-            body.style.backgroundColor = event.target.id
-        } else if (event.target.id === "yellow") {
-            body.style.backgroundColor = event.target.id
-        } else if (event.target.id === "purple") {
-            body.style.backgroundColor = event.target.id
+
+        // // // using if else
+        // // if (e.target.id === "grey") {
+        // //     body.style.backgroundColor = e.target.id;
+        // // } else if (e.target.id === "white") {
+        // //     body.style.backgroundColor = e.target.id;
+        // // } else if (e.target.id === "blue") {
+        // //     body.style.backgroundColor = e.target.id;
+        // // } else if (e.target.id === "yellow") {
+        // //     body.style.backgroundColor = e.target.id;
+        // // } else if (e.target.id === "purple") {
+        // //     body.style.backgroundColor = e.target.id;
+        // // }
+
+        // // // using switch case;
+        switch (e.target.id) {
+            case "grey":
+            case "white":
+            case "blue":
+            case "yellow":
+            case "purple":
+                body.style.backgroundColor = e.target.id;
+                break;
+            default:
+                break;
         }
 
-        // // using switch case
-        // switch (event.target.id) {
-        //     case "grey":
-        //     case "white":
-        //     case "blue":
-        //     case "yellow":
-        //     case "purple":
-        //         body.style.backgroundColor = event.target.id;
-        //         break;
-        //     default:
-        //         break;
-        // }
-
     });
-
 });
