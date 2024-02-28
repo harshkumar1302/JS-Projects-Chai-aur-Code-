fbd8315657e31519edb6477ddd712a18701a09a2
harshkumar1302 <bs824536@gmail.com>
2024-02-28 17:06:04 +0530

    Clock P3

--- 03_DigitalClock/script.js
+++ 03_DigitalClock/script.js
@@ -1,6 +1,14 @@
-const clock = document.getElementById('clock');
+const clock = document.querySelector("#clock")
 
 setInterval(function () {
     let date = new Date();
+    // console.log(date);
     clock.innerHTML = date.toLocaleTimeString();
 }, 1000)
+
+// const clock = document.getElementById('clock');
+
+// setInterval(function () {
+//     let date = new Date();
+//     clock.innerHTML = date.toLocaleTimeString();
+// }, 1000)
